c120ee7a62c192ad302c198a9d1f60c70937f46a
fortune-0 <fortunepeterspc07@gmail.com>
2023-03-12 18:08:54 -0700

    update

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    result = []
+  for num in range(len(my_list)):
+    if num % 2 == 0:
+      result.append(true)
+  else
+  result.append(false)
+
+  return result
