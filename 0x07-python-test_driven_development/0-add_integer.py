88b27e845e9c79548de08ea9a3f41683f571ebdb
fortune-0 <fortunepeterspc07@gmail.com>
2023-11-26 17:24:33 -0800

    updated'

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,7 +13,10 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
+    ''' the above line of checks if the supplied arguments is either an
+        integer or a float. if not raise an error message that a or b must be an integer
+        or a float.
+    '''
     
     #a = int(a) #if a float is passed convert a to an integer
     #b = int(b) #if a float is passed convert b to an integer
