38d62d149b453fb524d4c1b3349618532b5746d1
Fortune-0 <fortunepeterspc07@gmail.com>
2024-02-12 19:55:31 -0800

    multi line print

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const firstprint = ('C is fun');
+const secondprint = ('Python is cool');
+const thirdprint = ('JavaScript is amazing');
+console.log(firstprint);
+console.log(secondprint);
+console.log(thirdprint);
