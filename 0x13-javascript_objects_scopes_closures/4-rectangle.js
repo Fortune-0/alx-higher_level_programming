e567badceef45f02ad34fe6c740e52927ad37722
Fortune-0 <fortunepeterspc07@gmail.com>
2024-02-14 20:09:51 -0800

    created 4-rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+/**
+ * Check the parameters provided
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  rotate() {
+     // Swap the values of width and height using a temporary variable
+	let temp = this.width;
+	  this.width = this.height;
+	  this.height = temp;
+  }
+
+ double () {
+	 // multiples the width and the height of the rectangle by 2
+	this.width *= 2;
+	 this.height *= 2;
+]
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += 'X';
+        y++;
+      }
+
+      console.log(myVar);
+    }
+  }
+}
+module.exports = Rectangle;
