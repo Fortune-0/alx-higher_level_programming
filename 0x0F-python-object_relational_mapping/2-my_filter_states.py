99194b01048a007dbb53dd6d327dc184bd1918c0
Fortune-0 <fortunepeterspc07@gmail.com>
2024-02-19 03:17:13 +0100

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Module that list all the states from the hbtn_0e_0_usa
-"""
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    # Get MySQL credentials search name from command-line arguments
-    # and connect to MySQL servers
-    
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    
-    # execute the SQL query to retrive the specific name
-    c.execute(*SELECT * \
-        FROM `states` \
-            WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    # Fetch all rows and print the states
-    [print(state) for state in c.fetchall()]
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
