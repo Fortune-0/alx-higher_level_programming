b5e0194564a3e8a3c14c3c96ce2e6718fe54cf1a
Fortune-0 <fortunepeterspc07@gmail.com>
2024-02-29 20:53:37 -0800

    delete request

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+
+# Check if a URL is provided as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Store the URL from the command line argument
+URL="$1"
+
+# Use curl to get only the response header (-I option)
+# Look for Content-Length in the header
+# Extract the numeric part using awk
+size_in_bytes=$(curl -sI "$URL" | grep -i Content-Length | awk '{print $2}')
+
+# Display the size in bytes
+echo "Size of the response body: $size_in_bytes bytes"
+
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#get response vody for a given url for 200
+status code responses
+curl -sL "$1"
