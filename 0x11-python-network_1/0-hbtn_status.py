94c373bfcfde15c9b96b17a98c17115d2b68f2f7
Fortune-0 <fortunepeterspc07@gmail.com>
2024-03-01 18:27:24 -0800

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 import urllib.request
 
+# if __name__ == "__main__":
+#    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+#        body = response.read()
+#        print("Body response:")
+#        print("\t- type: {}".format(type(body)))
+#        print("\t- content: {}".format(body))
+#        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
