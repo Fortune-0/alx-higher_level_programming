35d7937cf6bafc06b8cbb057f9c8425469c30685
Fortune-0 <fortunepeterspc07@gmail.com>
2024-03-03 19:33:40 -0800

    update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,8 @@ import sys
 """Python script that takes in a url
     """
 
-if __name__=="__main__":
+if __name__ == "__main__":
+
     url = sys.argv[1]  # first argument is the URL to be checked
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
