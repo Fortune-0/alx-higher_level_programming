2d07145f12c7e560dd10fc9e52478726158647bd
Fortune-0 <fortunepeterspc07@gmail.com>
2024-03-04 04:38:15 +0100

    test

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,24 @@
+# #!/usr/bin/python3
+# import urllib.request
+# import sys
+# """Python script that takes in a url
+#     """
+
+# if __name__ == "__main__":
+
+#     url = sys.argv[1]  # first argument is the URL to be checked
+#     req = urllib.request.Request(url)
+#     with urllib.request.urlopen(req) as response:
+#         print(dict(response.headers).get("X-Request-Id"))
+
+
 #!/usr/bin/python3
 import urllib.request
 import sys
 """Python script that takes in a url
     """
 
-if __name__ == "__main__":
-
+if __name__=="__main__":
     url = sys.argv[1]  # first argument is the URL to be checked
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
